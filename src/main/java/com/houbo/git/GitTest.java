1ab0f3dafb3a5bcb0f210b89d20349a8395b69a9
houbo <houbo@houbo.com>
2023-02-17 15:34:31 +0800

    first commit

---
+++ src/main/java/com/houbo/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.houbo.git;
+
+/**
+ * @anthor houbo
+ * @create 2023-02-17-15:29
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("你好世界");
+    }
+}
